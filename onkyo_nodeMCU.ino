c50e31c09e47ba26fbfcf67217f8b3570170811c
Giova <giovanni.zilli@gmail.com>
2023-02-22 11:12:35 +0100

    added custom command

--- onkyo_nodeMCU.ino
+++ onkyo_nodeMCU.ino
@@ -13,8 +13,6 @@
 // RTC Memory Address for the DoubleResetDetector to use
 //#define DRD_ADDRESS 0
 
-// UART
-#define UART_SPEED 9600
 // ONKYO
 #define ONKYO_PIN 2  //13
 
@@ -30,8 +28,8 @@ bool testWifi(void);
 void launchWeb(void);
 void setupAP(void);
 
-//Establishing Local server at port 80 whenever required
 //DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
+
 ESP8266WebServer server(80);
 
 void setup() {
@@ -52,11 +50,8 @@ void setup() {
   Serial.println();
   Serial.println("Startup");
 
-
-
-  //---------------------------------------- Read EEPROM for SSID and pass
+  //-- Read EEPROM for SSID and pass
   Serial.println("Reading EEPROM ssid");
-
   String esid;
   for (int i = 0; i < 32; ++i) {
     esid += char(EEPROM.read(i));
@@ -73,8 +68,8 @@ void setup() {
   Serial.print("PASS: ");
   Serial.println(epass);
 
-
   WiFi.begin(esid.c_str(), epass.c_str());
+
   if (testWifi()) {
     Serial.println("Succesfully Connected!!!");
     if (MDNS.begin("onkyo_nodeMCU")) {
@@ -95,34 +90,33 @@ void setup() {
   }
 
   Serial.println();
-  Serial.println("Waiting.");
+  Serial.print("Waiting.");
 
   while ((WiFi.status() != WL_CONNECTED)) {
-    Serial.print(".");
-    delay(100);
     server.handleClient();
+    delay(1);
   }
 }
 
-//-------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change
+//-- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change
 bool testWifi(void) {
-  int c = 0;
+  uint i = 0;
   Serial.println("Waiting for Wifi to connect");
-  while (c < 20) {
+  while (i < 20) {
     if (WiFi.status() == WL_CONNECTED) {
       return true;
     }
     delay(500);
     Serial.print("*");
-    c++;
+    i++;
   }
-  Serial.println("");
+  Serial.println();
   Serial.println("Connect timed out, opening AP");
   return false;
 }
 
 void launchWeb() {
-  Serial.println("");
+  Serial.println();
   if (WiFi.status() == WL_CONNECTED)
     Serial.println("WiFi connected");
   Serial.print("Local IP: ");
@@ -136,84 +130,39 @@ void launchWeb() {
 }
 
 
-void onkyoSend(int command) {
-  onkyoWriteHeader();
-
-  for (int i = 0; i < 12; i++) {
-    bool level = command & 0x800;
-    command <<= 1;
-    onkyoWriteBit(level);
-  }
-
-  onkyoWriteFooter();
-}
-
-void onkyoWriteHeader() {
-  //Serial.println(micros());
-  digitalWrite(ONKYO_PIN, HIGH);
-  delayMicroseconds(3000);
-  digitalWrite(ONKYO_PIN, LOW);
-  delayMicroseconds(1000);
-  //Serial.println(micros());
-}
-void onkyoWriteBit(bool level) {
-  digitalWrite(ONKYO_PIN, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(ONKYO_PIN, LOW);
-
-  if (level)
-    delayMicroseconds(2000);
-  else
-    delayMicroseconds(1000);
-}
-
-void onkyoWriteFooter() {
-  digitalWrite(ONKYO_PIN, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(ONKYO_PIN, LOW);
-  delay(20);
-}
-
 void onkyoSendOn() {
-  server.send(200, "text/json", "{\"route\": \"on\",\"cmd\": \"0x02F\"}");
+  server.send(200, "application/json", "{\"route\": \"on\",\"cmd\": \"0x02F\"}");
   onkyoSend(0x02F);
 }
 
 void onkyoSendOff() {
-  server.send(200, "text/json", "{\"route\": \"off\",\"cmd\": \"0x0DA\"}");
+  server.send(200, "application/json", "{\"route\": \"off\",\"cmd\": \"0x0DA\"}");
   onkyoSend(0x0DA);
 }
 
 void onkyoSendMute() {
-  server.send(200, "text/json", "{\"route\": \"mute\",\"cmd\": \"0x005\"}");
+  server.send(200, "application/json", "{\"route\": \"mute\",\"cmd\": \"0x005\"}");
   onkyoSend(0x005);
 }
 
 void onkyoSendVolUp() {
-  server.send(200, "text/json", "{\"route\": \"volup\",\"cmd\": \"0x002\"}");
+  server.send(200, "application/json", "{\"route\": \"volup\",\"cmd\": \"0x002\"}");
   onkyoSend(0x002);
 }
 
 void onkyoSendVolDown() {
-  server.send(200, "text/json", "{\"route\": \"voldown\",\"cmd\": \"0x003\"}");
+  server.send(200, "application/json", "{\"route\": \"voldown\",\"cmd\": \"0x003\"}");
   onkyoSend(0x003);
 }
 
 void onkyoSendSource() {
-  server.send(200, "text/json", "{\"route\": \"source\",\"cmd\": \"0x0D5\"}");
+  server.send(200, "application/json", "{\"route\": \"source\",\"cmd\": \"0x0D5\"}");
   onkyoSend(0x0D5);
 }
 
-/*
-void onkyoSendCustom(char* cmd) {
-  server.send(200, "text/json", "{\"route\": \"source\",\"cmd\": \"" + cmd +"\"}");
-  unsigned long value = strtol(cmd, 0, 16);
-  onkyoSend(value);
-}
-*/
 // Serving Command List
 void getRoutesList() {
-  server.send(200, "text/json", "[{\"route\": \"on\",\"cmd\": \"0x02F\"}, {\"route\": \"off\",\"cmd\": \"0x0DA\"}, {\"route\": \"mute\",\"cmd\": \"0x005\"}, {\"route\": \"volup\",\"cmd\": \"0x002\"}, {\"route\": \"voldown\",\"cmd\": \"0x003\"}, {\"route\": \"source\",\"cmd\": \"0x0D5\"},{\"route\": \"initialize\",\"cmd\": \"Reset WiFi settings\"}]");
+  server.send(200, "application/json", "[{\"route\": \"on\",\"cmd\": \"0x02F\"}, {\"route\": \"off\",\"cmd\": \"0x0DA\"}, {\"route\": \"mute\",\"cmd\": \"0x005\"}, {\"route\": \"volup\",\"cmd\": \"0x002\"}, {\"route\": \"voldown\",\"cmd\": \"0x003\"}, {\"route\": \"source\",\"cmd\": \"0x0D5\"},{\"route\": \"initialize\",\"cmd\": \"Reset WiFi settings\"},{\"route\": \"custom\",\"URI parameters\": [{\"cmd\": []}]}]");
 }
 
 // Clear EEPROM
@@ -223,7 +172,7 @@ void clearEEPROM() {
   }
   delay(500);
   EEPROM.commit();
-  server.send(200, "text/json", "{\"route\": \"initialize\",\"cmd\": \"EEPROM ssid & pass data cleared!\"}");
+  server.send(200, "application/json", "{\"route\": \"initialize\",\"cmd\": \"EEPROM ssid & pass data cleared!\"}");
   Serial.println("EEPROM ssid & pass data cleared!");
   ESP.reset();
 }
@@ -231,13 +180,41 @@ void clearEEPROM() {
 // Define routing
 void restServerRouting() {
   server.on("/", HTTP_GET, getRoutesList);
-  server.on(F("/on"), HTTP_GET, onkyoSendOn);
-  server.on(F("/off"), HTTP_GET, onkyoSendOff);
-  server.on(F("/mute"), HTTP_GET, onkyoSendMute);
-  server.on(F("/volup"), HTTP_GET, onkyoSendVolUp);
-  server.on(F("/voldown"), HTTP_GET, onkyoSendVolDown);
-  server.on(F("/source"), HTTP_GET, onkyoSendSource);
-  server.on(F("/initialize"), HTTP_GET, clearEEPROM);
+  server.on("/on", HTTP_GET, onkyoSendOn);
+  server.on("/off", HTTP_GET, onkyoSendOff);
+  server.on("/mute", HTTP_GET, onkyoSendMute);
+  server.on("/volup", HTTP_GET, onkyoSendVolUp);
+  server.on("/voldown", HTTP_GET, onkyoSendVolDown);
+  server.on("/source", HTTP_GET, onkyoSendSource);
+  server.on("/initialize", HTTP_GET, clearEEPROM);
+
+  server.on("/custom", []() {
+    String cmd = server.arg("cmd");
+
+    if (cmd.length() == 0) {
+      content = "{\"Error\":\"422 required parameter not found\"}";
+      statusCode = 422;
+    } else {
+      int cmd_len = cmd.length() + 1;
+      char cmd_array[cmd_len];
+      cmd.toCharArray(cmd_array, cmd_len);
+      int cmd_int = (int)strtol(cmd_array, 0, 16);
+
+      if (cmd_int != 0) {
+        //Serial.println(cmd_array);
+        //Serial.println(cmd_int);
+        onkyoSend(cmd_int);
+
+        content = "{\"route\": \"custom\",\"cmd\": \"" + cmd + "\"}";
+        statusCode = 200;
+      } else {
+        content = "{\"Error\":\"400 Bad request\"}";
+        statusCode = 400;
+      }
+    }
+    //server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(statusCode, "application/json", content);
+  });
 }
 
 // Manage not found URL
@@ -279,9 +256,8 @@ void setupAP(void) {
       delay(10);
     }
   }
-  Serial.println("");
+  Serial.println();
   st = "<ol>";
-  //selectSSID = "<select id='ssid' name='ssid'>";
   selectSSID = "<input type='text' id='ssid' name='ssid' list='ssidList'/><datalist id='ssidList'>";
   for (int i = 0; i < n; ++i) {
     // Print SSID and RSSI for each network found
@@ -300,7 +276,6 @@ void setupAP(void) {
     st += "</li>";
   }
   st += "</ol>";
-  //selectSSID += "</select>";
   selectSSID += "</datalist>";
   delay(100);
   WiFi.softAP("onkyo_nodeMCU", "");
@@ -325,11 +300,10 @@ void createWebServer() {
     server.send(200, "text/html", content);
   });
   server.on("/scan", []() {
-    //setupAP();
     IPAddress ip = WiFi.softAPIP();
     String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
 
-    content = "<!DOCTYPE HTML>\r\n<html>go back";
+    content = "<!DOCTYPE HTML>\r\n<html><button onclick='window.location=\"192.168.1.4\";'>Back</button></html>";
     server.send(200, "text/html", content);
   });
 
@@ -342,9 +316,9 @@ void createWebServer() {
         EEPROM.write(i, 0);
       }
       Serial.println(qsid);
-      Serial.println("");
+      Serial.println();
       Serial.println(qpass);
-      Serial.println("");
+      Serial.println();
 
       Serial.println("writing eeprom ssid:");
       for (int i = 0; i < qsid.length(); ++i) {
@@ -371,6 +345,45 @@ void createWebServer() {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(statusCode, "application/json", content);
   });
+}
+
+/*ONKYO*/
+void onkyoSend(int command) {
+  onkyoWriteHeader();
+
+  for (int i = 0; i < 12; i++) {
+    bool level = command & 0x800;
+    command <<= 1;
+    onkyoWriteBit(level);
+  }
+
+  onkyoWriteFooter();
+}
+
+void onkyoWriteHeader() {
+  //Serial.println(micros());
+  digitalWrite(ONKYO_PIN, HIGH);
+  delayMicroseconds(3000);
+  digitalWrite(ONKYO_PIN, LOW);
+  delayMicroseconds(1000);
+  //Serial.println(micros());
+}
+void onkyoWriteBit(bool level) {
+  digitalWrite(ONKYO_PIN, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(ONKYO_PIN, LOW);
+
+  if (level)
+    delayMicroseconds(2000);
+  else
+    delayMicroseconds(1000);
+}
+
+void onkyoWriteFooter() {
+  digitalWrite(ONKYO_PIN, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(ONKYO_PIN, LOW);
+  delay(20);
 }
 
 void loop(void) {
